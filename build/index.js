5428531c35dadd8092d154bb31afc8ba80849d76
Edgar Ortiz <enob730@gmail.com>
2022-04-14 19:07:55 -0600

    Actualizacion cambios prod levantado

--- build/index.js
+++ build/index.js
@@ -4,13 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const rebels_1 = __importDefault(require("./routes/rebels"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 const PORT = 3000;
-app.get('/ping', (_req, res) => {
-    console.log('Hola mundo');
-    res.send();
-});
+app.use('/', rebels_1.default);
 app.listen(PORT, () => {
-    console.log('Servidor corriendo'+11122);
+    console.log('Servidor corriendo');
 });
